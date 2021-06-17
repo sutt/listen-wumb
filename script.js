0ffd386817a5ce36186fce48f0977f66d9f70add
Will Sutton <wsutton17@gmail.com>
2021-06-17 11:00:11 -0400

    parsing the dom of the playlist page; janky

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 console.log("hello js")
 
 const url = "http://127.0.0.1:3000/parse"
+let response = null
 
 fetch(url)
     .then(res => {
-        response = res   
-        // console.log(res)
-        // console.log(res.body)
+        res.text()   
+            .then(body => {
+                
+                response = body
+
+                const stage = document.createElement("div")
+                stage.innerHTML = body
+                
+                const tbs = stage.querySelector("#MainContentTextOnly").querySelectorAll("tbody")
+
+                const data = Array.from(tbs).map( tb => {
+                    return {
+                        time: tb.children[0].innerText.split("\t")[0], 
+                        artist: tb.children[0].innerText.split("\t")[1], 
+                        title: tb.children[1].innerText
+                    }
+                })
+
+                console.log(data)
+            })
     })

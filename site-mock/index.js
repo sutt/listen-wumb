2b639546015a78e5fb1743d447b3c9c593cb6044
Will Sutton <wsutton17@gmail.com>
2021-10-02 17:07:17 -0400

    sitemock now parses the mock POST request from proxy-2 using body-parser pkg

--- site-mock/index.js
+++ site-mock/index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const fs = require('fs')
 const app = express()
 
 app.use(cors())
+app.use(bodyParser.urlencoded(
+    {type:'application/x-www-form-urlencoded',
+    extended:false}))
 app.set('port', process.env.PORT || 3005 )
+
+const debug = true
 
 // load jsons fro mock yt-search
 const DATA_DIR = './yt-mock-data'
@@ -40,7 +46,7 @@ app.get("/page-new", (req, res) => {
     const availablePages = ['210928', '210901']
     
     const fn = availablePages.includes(postSearchDate)
-                ? `${dParam}.html`
+                ? `${postSearchDate}.html`
                 : `210928.html`
     
     res.sendFile(
@@ -49,8 +55,23 @@ app.get("/page-new", (req, res) => {
     )
 })
 
-app.put("/page-new", (req, res) => {
-    postSearchDate = req.body['acf[field_5e4bac0c25353]']
+app.post("/page-new", (req, res) => {
+    
+    if (debug) console.log("post is hit---------------------------------")
+
+    try {
+        postSearchDate = req.body['acf[field_5e4bac0c25353]']
+        postSearchDate = postSearchDate.slice(2)
+    
+        if (debug) console.log(`postSeachDate set to: ${postSearchDate}`)
+    
+    } catch {
+        if (debug) {
+            // this might be because parsing a x-www-form-urlencoded body
+            console.log("couldnt find date in request body")
+            console.log(req.body)
+        }
+    }
     res.send("ok")
 })
 
